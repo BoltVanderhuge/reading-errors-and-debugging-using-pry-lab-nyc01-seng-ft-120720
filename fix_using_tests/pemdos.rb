625792364e60d8a2a420acbc26cfcdd7c6fcd5d2
John Wisneski <john.j.wisneski@gmail.com>
2020-11-18 03:43:57 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,8 @@
 # don't forget to add: require 'pry'
 
 def snake_it_up(string)
-  if string[0] == "s"
-    10 * "s".toi + string
+  if string[0.tos] == "s"
+    10 * "s" + string
   else
     string
   end
